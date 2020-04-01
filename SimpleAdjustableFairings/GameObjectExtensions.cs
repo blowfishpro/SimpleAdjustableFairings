9a2100b829eeeecfa4b31272f418fedc500183c4
blowfish <blowfishpro@users.noreply.github.com>
2020-03-31 23:21:19 -0700

    Include disabled components in searches
    Fixes some model switching issues (most visibly with transparency)

--- SimpleAdjustableFairings/GameObjectExtensions.cs
+++ SimpleAdjustableFairings/GameObjectExtensions.cs
@@ -6,7 +6,7 @@ namespace SimpleAdjustableFairings
     {
         public static void MakeTransparent(this GameObject gameObject, float opacity = 0.5f)
         {
-            foreach (MeshRenderer meshRenderer in gameObject.GetComponentsInChildren<MeshRenderer>())
+            foreach (MeshRenderer meshRenderer in gameObject.GetComponentsInChildren<MeshRenderer>(true))
             {
                 meshRenderer.material.renderQueue = 6000;
                 meshRenderer.material.SetFloat(PropertyIDs._Opacity, opacity);
@@ -15,7 +15,7 @@ namespace SimpleAdjustableFairings
 
         public static void MakeOpaque(this GameObject gameObject)
         {
-            foreach (MeshRenderer meshRenderer in gameObject.GetComponentsInChildren<MeshRenderer>())
+            foreach (MeshRenderer meshRenderer in gameObject.GetComponentsInChildren<MeshRenderer>(true))
             {
                 meshRenderer.material.renderQueue = -1;
                 meshRenderer.material.SetFloat(PropertyIDs._Opacity, 1f);
@@ -24,7 +24,7 @@ namespace SimpleAdjustableFairings
 
         public static void SetCollidersEnabled(this GameObject gameObject, bool enabled)
         {
-            foreach (Collider collider in gameObject.GetComponentsInChildren<Collider>())
+            foreach (Collider collider in gameObject.GetComponentsInChildren<Collider>(true))
             {
                 collider.enabled = enabled;
             }
